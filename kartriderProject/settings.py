acec6c76dd6fb84cd078dfb03f96dad11b32cf4b
hooong <tjrwns0529@gmail.com>
2020-12-10 15:59:35 +0900

    feat: get_user_access_id

--- kartriderProject/settings.py
+++ kartriderProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +126,8 @@ STATICFILES_DIRS = [
     STATIC_URL
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, '../templates/static')
+
+#API Key
+with open("/Users/hooong/key.json") as f:
+    keyfile = json.load(f)
+API_KEY = keyfile["key"]
--- search_rider/views.py
+++ search_rider/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
+URL = "https://api.nexon.co.kr/kart/v1.0/"
+headers = {"Authorization": settings.API_KEY}
 
 
 def index(request):
+    print(get_access_id("SFll폭주M"))
     return render(request, "home.html", {})
+
+
+def search(request):
+    return
+
+
+def get_access_id(nickname):
+    res = requests.get(URL + "users/nickname/" + nickname, headers=headers)
+    # res.encoding = 'utf-8'
+    res = res.json()
+
+    return res["accessId"]
